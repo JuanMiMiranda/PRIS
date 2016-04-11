12395552c8b357b5ae51cd9f8f67e6f6732851f5
Juan <Juan@JuanMi>
2016-04-11 18:05:58 +0200

    Extracting Frequent Renter Points

--- Refactoring/src/Chapter01/Customer.java
+++ Refactoring/src/Chapter01/Customer.java
@@ -25,24 +25,22 @@ public class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			Rental each = (Rental) rentals.nextElement();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
+		Rental each = (Rental) rentals.nextElement();
+		frequentRenterPoints += each.getFrequentRenterPoints();
+		//show figures for this rental
+		result += "\t" + each.getMovie().getTitle()+ "\t" +
+		String.valueOf(each.getCharge()) + "\n";
+		totalAmount += each.getCharge();
 		}
-		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		//add footer lines
+		result += "Amount owed is " + String.valueOf(totalAmount) +
+		"\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
+		+
+		" frequent renter points";
 		return result;
-	}
+		}
+		
 
 	private double amountFor(Rental aRental) {
 		return aRental.getCharge();
